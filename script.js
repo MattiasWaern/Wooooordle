38b440c4c806b397e49b9050c952de9908f0d940
ReggeMeister <mattiaswaern04@gmail.com>
2025-12-08 15:49:13 +0100

    commit

--- script.js
+++ script.js
@@ -15289,8 +15289,16 @@ const dictionary = [
   "rural",
   "shave"
 ]
+const WORD_LENGTH = 5;
+const FLIP_ANIMATION_DURATION = 500
+const keyboard = document.querySelector("[data-keyboard]");
+const alertContainer = document.querySelector("[data-alert-container]");
+const guessGrid = document.querySelector("[data-guess-grid]");
+const offsetFromDate = new Date(2025, 13, 8)
+const msOffset = Date.now() - offsetFromDate
+const dayOffset = msOffset / 1000 / 60 / 60 / 24
+const targetWord = targetWords[Math.floor(dayOffset)]
 
-const guessGrid = document.querySelector("data-guess-grid");
 
 function startInteraction(){
   document.addEventListener("click", handleMouseClick);
@@ -15337,5 +15345,79 @@ function handleKeyPress(e){
 }
 
 function pressKey(key){
+  const activeTiles = getActiveTiles ()
+  if (activeTiles.length >= WORD_LENGTH) return;
+  const nextTile = guessGrid.querySelector(":not([data-letter])")
+  nextTile.dataset.letter = key.toLowerCase()
+  nextTile.textContent = key
+  nextTile.dataset.state = "active"
+}
 
+function deleteKey(){
+  const activeTiles = getActiveTiles ()
+  const lastTile = activeTiles[activeTiles.length -1]
+  if (lastTile == null) return
+  lastTile.textContent = ""
+  delete lastTile.dataset.state
+  delete lastTile.dataset.letter
+}
+
+function sumbitGuess(){
+  const activeTiles = [...getActiveTiles()]
+  if (activeTiles.length !== WORD_LENGTH){
+    showAlert("Not enough letters")
+    shakeTiles(activeTiles)
+    return
+  }
+
+  const guess = activeTiles.reduice((word, tile) => {
+    return word + tile.dataset.letter
+  }, "")
+  console.log(guess)
+
+  if (!dictionary.includes(guess)) {
+    showAlert("Not in word list!")
+    shakeTiles(activeTiles)
+    return
+  }
+
+  stopInteraction()
+  activeTiles.forEach((..params) => flipTile(..params, guess))
+  
+}
+
+function flipTile(tile, index, array, guess){
+  const letter = tile.dataset.letter
+  const key = Keyboard.querySelector(`[data-key="${letter}"]`)
+  setTimeout(() => {
+    tile.classList.add("flip")
+  }, index * FLIP_ANIMATION_DURATION / 2)
+}
+
+function getActiveTiles(){
+  return guessGrid.querySelectorAll('[data-state="active"]')
+}
+
+function showAlert(message, duration = 1000){
+  const alert = document.createElement("div")
+  alert.textContent = message
+  alert.classList.add("alert")
+  alertContainer.prepend(alert)
+  if (duration == null) return
+
+  setTimeout(() => {
+    alert.classList.add("hide")
+    alert.addEventListener("transitionend", () => {
+      alert.remove()
+    })
+  }, duration)
+}
+
+function shakeTiles(tiles){
+  tiles.forEach(tile => {
+    tiles.classList.add("shake")
+    tile.addEventListener("animationend", () => {
+      tiles.classList.remove("shake")
+  },  {once: true})
+})
 }
