19a1356707d5da926258b58b7868a2581f831b35
ReggeMeister <mattiaswaern04@gmail.com>
2025-12-08 14:42:50 +0100

    skapade relevanta funktioner
    -startInteraction
    -stopInteraction
    -handleMouseClick
    -handleKeyPress

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const targetWord = [
+const targetWords = [
   "cigar",
   "rebut",
   "sissy",
@@ -15290,3 +15290,52 @@ const dictionary = [
   "shave"
 ]
 
+const guessGrid = document.querySelector("data-guess-grid");
+
+function startInteraction(){
+  document.addEventListener("click", handleMouseClick);
+  document.addEventListener("keydown", handleKeyPress);
+}
+
+function stopInteraction(){
+  document.removeEventListener("click", handleMouseClick);
+  document.removeEventListener("keydown", handleKeyPress);
+}
+
+function handleMouseClick(e){
+  if(e.target.matches("[data-key]")){
+    pressKey(e.target.dataset.key)
+    return;
+  }
+
+  if (e.target.matches("[data-enter]")){
+    submitGuess();
+    return;
+  }
+
+  if (e.target.matches("[data-delete]")){
+    deleteKey();
+    return;
+  }
+}
+
+function handleKeyPress(e){
+  if (e.key === "Enter"){
+    submitGuess();
+    return;
+  }
+
+  if (e.key === "Backspace" || e.key === "Delete"){
+    deleteKey();
+    return;
+  }
+
+  if (e.key.match(/^[a-z]$/)){
+    pressKey(e.key);
+    return;
+  }
+}
+
+function pressKey(key){
+
+}
